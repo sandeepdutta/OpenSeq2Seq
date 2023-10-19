79c534da78669d0cd39d7a78670b58096c377c00
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-10-19 09:43:59 -0700

    revered change

--- decoders/setup.py
+++ decoders/setup.py
@@ -3,9 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from setuptools import setup, Extension
-import setuptools
-from distutils import ccompiler
+from setuptools import setup, Extension, distutils
 import glob
 import platform
 import os, sys, shutil

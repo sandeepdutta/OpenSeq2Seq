875edf07900da8963628ecd5b1fcab051dec6c6d
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-10-19 09:35:17 -0700

    updating for updated setup tools

--- decoders/setup.py
+++ decoders/setup.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from setuptools import setup, Extension, distutils
+from setuptools import setup, Extension
+import setuptools
+from distutils import ccompiler
 import glob
 import platform
 import os, sys, shutil
